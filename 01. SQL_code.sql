c876fdc2dd452fae354a437e6e3e98eceae3d35f
Ako <115475171+Anibladze1@users.noreply.github.com>
2022-11-14 01:04:43 +0400

    sql code and diagram picture upload

--- 01. SQL_code.sql
+++ 01. SQL_code.sql
@@ -1,49 +1,49 @@
 CREATE TABLE `undergraduates` (
   `id` int PRIMARY KEY,
-  `fname` varchar(255),
-  `lname` varchar(255),
-  `Personal_ID` varchar(255),
-  `phone_number` varchar(255),
-  `email` varchar(255),
-  `address` varchar(255),
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `Personal_ID` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `secondAdress` varchar(255),
   `houseNumber` varchar(255),
-  `birthdate` date,
-  `sex` boolean,
-  `schools_id` int
+  `birthdate` date, NOT NULL
+  `sex` boolean NOT NULL,
+  `schools_id` int NOT NULL
 );
 
-CREATE TABLE `proffessors` (
+CREATE TABLE `professors` (
   `id` int PRIMARY KEY,
-  `fname` varchar(255),
-  `lname` varchar(255),
-  `Personal_ID` varchar(255),
-  `phone_number` varchar(255),
-  `email` varchar(255),
-  `address` varchar(255),
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `Personal_ID` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `secondAdress` varchar(255),
   `houseNumber` varchar(255),
-  `birthdate` date,
-  `sex` boolean,
-  `bank_requisites` varchar(255),
-  `title` varchar(255),
-  `managers_id` int
+  `birthdate` date NOT NULL,
+  `sex` boolean NOT NULL,
+  `bank_requisites` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `managers_id` int NOT NULL
 );
 
 CREATE TABLE `managers` (
   `id` int PRIMARY KEY,
-  `fname` varchar(255),
-  `lname` varchar(255),
-  `personal_ID` varchar(255),
-  `phone_number` varchar(255),
-  `email` varchar(255),
-  `address` varchar(255),
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `personal_ID` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `secondAdress` varchar(255),
   `houseNumber` varchar(255),
-  `birthdate` date,
-  `sex` boolean,
-  `bank_requisites` varchar(255),
-  `title` varchar(255)
+  `birthdate` date NOT NULL,
+  `sex` boolean NOT NULL,
+  `bank_requisites` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL
 );
 
 CREATE TABLE `schools` (
@@ -53,33 +53,33 @@ CREATE TABLE `schools` (
 
 CREATE TABLE `subjects` (
   `id` int PRIMARY KEY,
-  `subject_name` varchar(255),
-  `schools_id` int
+  `subject_name` varchar(255) NOT NULL,
+  `schools_id` int NOT NULL
 );
 
 CREATE TABLE `lecture_time` (
   `id` int PRIMARY KEY,
-  `subjects_id` int,
-  `time` varchar(255),
-  `weekday` varchar(255)
+  `subjects_id` int NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `weekday` varchar(255) NOT NULL
 );
 
 CREATE TABLE `subjects_of_undergraduates` (
   `id` int PRIMARY KEY,
-  `undergraduates_id` int,
-  `subjects_id` int
+  `undergraduates_id` int NOT NULL,
+  `subjects_id` int NOT NULL
 );
 
 CREATE TABLE `subjects_of_professors` (
   `id` int PRIMARY KEY,
-  `proffessors_id` int,
-  `subjects_id` int
+  `professors_id` int NOT NULL,
+  `subjects_id` int NOT NULL
 );
 
 CREATE TABLE `managers_schools` (
   `id` int PRIMARY KEY,
-  `managers_id` int,
-  `schools_id` int
+  `managers_id` int NOT NULL,
+  `schools_id` int NOT NULL
 );
 
 ALTER TABLE `undergraduates` ADD FOREIGN KEY (`schools_id`) REFERENCES `schools` (`id`);
@@ -92,7 +92,7 @@ ALTER TABLE `subjects_of_undergraduates` ADD FOREIGN KEY (`undergraduates_id`) R
 
 ALTER TABLE `subjects_of_undergraduates` ADD FOREIGN KEY (`subjects_id`) REFERENCES `subjects` (`id`);
 
-ALTER TABLE `subjects_of_professors` ADD FOREIGN KEY (`proffessors_id`) REFERENCES `proffessors` (`id`);
+ALTER TABLE `subjects_of_professors` ADD FOREIGN KEY (`professors_id`) REFERENCES `professors` (`id`);
 
 ALTER TABLE `subjects_of_professors` ADD FOREIGN KEY (`subjects_id`) REFERENCES `subjects` (`id`);
 
